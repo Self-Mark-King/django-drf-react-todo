3f4c37e749b0bf9585df99ae820e4aa56be09bd7
Self-Mark-King <mg_king@hotmail.com>
2019-12-04 19:35:13 +0000

    bugfix:  change urls.py to add path to index

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -23,5 +23,6 @@ router.register(r'todos', views.TodoView, 'todo')     # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))                # add this
+    path('api/', include(router.urls)),
+    path('frontend/public/index.html')              # add this
 ]
