76790486759d473309a273eeb72ea56c20c898ec
Self-Mark-King <mg_king@hotmail.com>
2019-12-06 19:03:35 +0000

    error: frontend doesnt work on master

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'; // added from tutorial
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
