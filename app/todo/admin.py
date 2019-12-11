fcdd386e28c0ba9296d5ebb302394ac4cf4b8461
Self-Mark-King <mg_king@hotmail.com>
2019-12-11 22:06:19 +0000

    create person model

--- app/todo/admin.py
+++ app/todo/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Todo # add this
+from .models import person
 
 class TodoAdmin(admin.ModelAdmin):  # add this
     list_display = ('title', 'description', 'completed') # add this
 
+class personAdmin(admin.ModelAdmin):  # add this
+    list_display = ('first_name', 'last_name') # add this
+
 # Register your models here.
 admin.site.register(Todo, TodoAdmin) # add this
+admin.site.register(person, personAdmin)
--- app/todo/models.py
+++ app/todo/models.py
@@ -9,3 +9,11 @@ class Todo(models.Model):
 
     def _str_(self):
         return self.title
+
+class person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name
+
