adb875120468bdb56647eb3f701bb68d445adbaa
Self-Mark-King <mg_king@hotmail.com>
2019-12-12 18:52:52 +0000

    reapair db

--- app/todo/models.py
+++ app/todo/models.py
@@ -2,18 +2,21 @@ from django.db import models
 # Create your models here.
 
 # add this
-class Todo(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
-
-    def _str_(self):
-        return self.title
-
 class person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.first_name
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    Person = models.ForeignKey(person, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.title
+
 
